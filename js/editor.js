55ca683b706a7c7921a9195a0842e9857c59dc71
Luka Waymouth <lukawaymouth@gmail.com>
2023-12-09 08:44:19 -0500

    Modify qualifiers code to allow for multiple form instances

--- js/editor.js
+++ js/editor.js
@@ -1,14 +1,18 @@
 jQuery(document).ready(function($) {
-	qualifiers_form = sgssd_forms_attach($, $("#sgssd_lists"),
-		$("#sgssd_segments"), $("#sgssd_all_contacts"), $("#sgssd_group"),
-		$("#sgssd_sender"), $("#sgssd_reload"), $("#sgssd_loading"));
+	qualifiers_form = sgssd_forms.attach($, {}, $(), $(), $("#sgssd_lists"),
+		$("#sgssd_segments"), $("#sgssd_all_contacts"),
+		$("#sgssd_group"),$("#sgssd_sender"));
+
+	$("#sgssd_reload").on("click", function() {
+		sgssd_forms.update();
+	});
 
 	$("#sgssd_create").on("click", function() {
 		$.post(sgssd_editor_ajax.ajax_url, {
 			_ajax_nonce: sgssd_editor_ajax.create_nonce,
 			action: "sgssd_create",
 			post_ID: $('#post_ID').val(),
-			qualifiers: JSON.stringify(qualifiers_form.result()),
+			qualifiers: JSON.stringify(qualifiers_form.compile()),
 			should_schedule: "false",
 		});
 	});
@@ -18,7 +22,7 @@ jQuery(document).ready(function($) {
 			_ajax_nonce: sgssd_editor_ajax.create_nonce,
 			action: "sgssd_create",
 			post_ID: $('#post_ID').val(),
-			qualifiers: JSON.stringify(qualifiers_form.result()),
+			qualifiers: JSON.stringify(qualifiers_form.compile()),
 			should_schedule: "true",
 		});
 	});
--- js/forms.js
+++ js/forms.js
@@ -1,66 +1,112 @@
-function sgssd_forms_attach($, lists, segments, all_contacts,
-		suppression_group, sender, reload, loading) {
-	containers = lists.add(segments.add(suppression_group.add(sender)));
-	updating = false
-	update = function() {
-		containers.children().remove();
-		reload.hide();
-		loading.show();
+const sgssd_forms = (function(){
+	let qualifiers_state = null;
+	const update_event = new Event("sgssd-forms-update");
 
-		if(updating) return;
-		updating = true;
-		$.get(sgssd_forms_ajax.ajax_url, {
+	let int_option = function(q) {
+		let node = q.get(0);
+		let i = node.selectedIndex;
+		if(i < 0) return null;
+		return Number(node.options[i].value);
+	};
+
+	let do_update = function() {
+		qualifiers_state = null;
+		document.dispatchEvent(update_event);
+
+		jQuery.get(sgssd_forms_ajax.ajax_url, {
 			_ajax_nonce: sgssd_forms_ajax.get_qualifiers_nonce,
 			action: "sgssd_get_qualifiers",
 		}, function(data) {
-			for(a of data.lists) {
-				lists.append(
-					`<input type="checkbox" value="${a.id}">
-						<span>${a.name}</span><br>`);
-			}
-			for(a of data.segments) {
-				segments.append(
-					`<input type="checkbox" value="${a.id}">
-						<span>${a.name}</span><br>`);
-			}
-			for(a of data.suppressions) {
-				suppression_group.append(
-					`<option value="${a.id}">${a.name}</option>`);
-			}
-			for(a of data.senders) {
-				sender.append(
-					`<option value="${a.id}">
-						${a.nickname}: ${a.from.name}
-						&lt;${a.from.email}&gt;</option>`);
-			}
-
-			updating = false;
-			reload.show();
-			loading.hide();
+			qualifiers_state = data;
+			document.dispatchEvent(update_event);
 		}, "json");
 	};
-
-	update();
-	reload.on("click", function() {
-		update();
-	});
-
-	int_option = function(q) {
-		node = q.get(0);
-		return Number(node.options[node.selectedIndex].value);
-	};
+	do_update();
 
 	return {
-		result: function() {
+		attach: function($, state, loading, not_loading, lists, segments,
+				all_contacts, suppression_group, sender) {
+			let containers = lists.add(segments.add(suppression_group.add(sender)));
+
+			state.lists ||= [];
+			state.suppressions ||= [];
+			state.all_conotacts ||= false;
+
+			let compile = function() {
+				return {
+					lists: lists.children("input:checked")
+						.get().map((a) => a.value),
+					segments: segments.children("input:checked")
+						.get().map((a) => a.value),
+					suppression_group: int_option(suppression_group),
+					sender: int_option(sender),
+					all_contacts: all_contacts.get(0).checked,
+				}
+			}
+
+			let when_loading = function() {
+				state = compile();
+				loading.show();
+				not_loading.hide();
+				containers.children().remove();
+			}
+
+			let after_loading = function() {
+				for(a of qualifiers_state.lists) {
+					let checked = (state.lists.includes(a.id))?("checked"):("");
+					lists.append(
+						`<input type="checkbox" value="${a.id}" ${checked}>
+						<span>${a.name}</span><br>`);
+				}
+
+				for(a of qualifiers_state.segments) {
+					let checked = (state.segments.includes(a.id))?("checked"):("");
+					segments.append(
+						`<input type="checkbox" value="${a.id}" ${checked}>
+						<span>${a.name}</span><br>`);
+				}
+
+				for(i in qualifiers_state.suppressions) {
+					a = qualifiers_state.suppressions[i]
+					suppression_group.append(
+						`<option value="${a.id}">${a.name}</option>`);
+					if(state.suppression_group == a.id) {
+						suppression_group.get(0).selectedIndex = i;
+					}
+				}
+
+				for(i in qualifiers_state.senders) {
+					a = qualifiers_state.senders[i]
+					sender.append(
+						`<option value="${a.id}">
+						${a.nickname}: ${a.from.name}
+						&lt;${a.from.email}&gt;</option>`);
+					if(state.senders == a.id) {
+						sender.get(0).selectedIndex = i;
+					}
+				}
+
+				loading.hide();
+				not_loading.show();
+			}
+
+			on_update = function() {
+				if(qualifiers_state == undefined) {
+					when_loading();
+				} else {
+					after_loading();
+				}
+			};
+			on_update();
+			jQuery(document).on("sgssd-forms-update", on_update);
+
 			return {
-				lists: lists.children("input:checked")
-					.get().map((a) => a.value),
-				segments: segments.children("input:checked")
-					.get().map((a) => a.value),
-				suppression_group: int_option(suppression_group),
-				sender: int_option(sender),
-				all_contacts: all_contacts.get(0).checked,
+				compile: compile,
 			};
 		},
+
+		update: function() {
+			if(qualifiers_state != null) do_update();
+		},
 	};
-}
+})();
